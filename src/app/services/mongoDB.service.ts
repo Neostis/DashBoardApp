ca1b782a509f400c0d01eda430b51806d0290d9d
Neostis <108649813+Neostis@users.noreply.github.com>
2023-11-27 13:25:59 +0700

    init

---
+++ src/app/services/mongoDB.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { MongoClient, Db, Collection, FindCursor, Document } from 'mongodb';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MongoDBService {
+  private url =
+    'mongodb://myUserAdmin:5678@localhost:27017/DashBoardDB?authSource=admin';
+  private dbName = 'DashBoardDB';
+  private client: MongoClient;
+
+  constructor() {
+    this.client = new MongoClient(this.url);
+  }
+
+  async connectToMongoDB(): Promise<Db> {
+    try {
+      await this.client.connect();
+      return this.client.db(this.dbName);
+    } catch (err) {
+      console.error('Error connecting to MongoDB:', err);
+      throw err;
+    }
+  }
+
+  getCollection(db: Db, collectionName: string): Collection {
+    return db.collection(collectionName);
+  }
+
+  async insertDocument(collection: Collection, document: any): Promise<any> {
+    return collection.insertOne(document);
+  }
+
+  async findDocuments(collection: Collection, query: any): Promise<Document[]> {
+    const cursor: FindCursor<Document> = collection.find(query);
+    return cursor.toArray();
+  }
+
+  async closeConnection(): Promise<void> {
+    await this.client.close();
+  }
+}
