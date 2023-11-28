d93c56019e7770cb39fdc209cb2f3062ea2141cf
Jarpip <naluedon.a@gmail.com>
2023-11-28 13:41:01 +0700

    Add Dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },  {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.page').then( m => m.DashboardPage)
   },
+
 ];
