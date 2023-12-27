47c0993099ab11848f4cc6a0df5b1fa0d1dcfb40
Jarpip <naluedon.a@gmail.com>
2023-12-27 16:52:48 +0700

    Finish all input for payment

--- src/app/payments/payments.component.ts
+++ src/app/payments/payments.component.ts
@@ -36,6 +36,8 @@ export class PaymentsComponent implements OnInit {
   _ProjectId!: string;
   projectList: ProjectModel[] = [];
 
+  isHidden: boolean = true;
+
   testPayment: any;
 
   constructor(
@@ -46,14 +48,17 @@ export class PaymentsComponent implements OnInit {
     this.form = this.formBuilder.group({
       input1: 'Precise',
       input2: '',
-      input3: '',
-      input4: true,
+      input3: 0,
+      input4: false,
       input5: false,
-      input6: true,
+      input6: false,
     });
   }
   ngOnInit(): void {
     this.loadProject();
+    // this.form.valueChanges.subscribe((f) => {
+    //   console.log(f);
+    // });
   }
 
   private loadProject(): void {
@@ -74,27 +79,35 @@ export class PaymentsComponent implements OnInit {
   }
 
   private getPayment(): void {
-    this.mongoDBService
-      .getPayments(this.sharedService.useProjectVariable()._id)
-      .subscribe({
-        next: (response : any) => {
-          // Call the presentToast function
-          this.testPayment = response.payments;
-          console.log('Payments:', response.payments[0]);
-        },
-        error: (error) => {
-          // Handle error
-          console.error('Error fetching payments:', error);
-        },
-        complete: () => {
-          // Handle completion if needed
-        },
-      });
+    this.mongoDBService.getPayments(this._ProjectId).subscribe({
+      next: (response: any) => {
+        // Call the presentToast function
+        this.testPayment = response.payments[0];
+        // console.log('Payments:', response.payments[0]);
+      },
+      error: (error) => {
+        // Handle error
+        console.error('Error fetching payments:', error);
+      },
+      complete: () => {
+        console.log(this.testPayment);
+        this.form.get('input1')?.setValue(this.testPayment.usage);
+        this.form.get('input2')?.setValue(this.testPayment.note);
+        this.form.get('input3')?.setValue(parseInt(this.testPayment.budget));
+        if (this.testPayment.notification.includes('Email')) {
+          this.form.get('input4')?.setValue(true);
+        }
+        if (this.testPayment.notification.includes('Phone')) {
+          this.form.get('input5')?.setValue(true);
+        }
+        this.form.get('input6')?.setValue(this.testPayment.change);
+      },
+    });
   }
 
   private updatePayment(data: PaymentModel): void {
     this.mongoDBService.updatePayment(data).subscribe({
-      next: (response : any) => {
+      next: (response: any) => {
         // Call the presentToast function
         console.log('Payment updated successfully:', response);
       },
@@ -103,6 +116,7 @@ export class PaymentsComponent implements OnInit {
         console.error('Error fetching payments:', error);
       },
       complete: () => {
+        this.getPayment();
         // Handle completion if needed
       },
     });
@@ -116,27 +130,32 @@ export class PaymentsComponent implements OnInit {
       this.notificationList.push('Phone');
     }
 
-    const data: PaymentModel = {
-      usage: this.form.value.input1,
-      note: this.form.value.input1,
-      budget: this.form.value.input1,
-      change: this.form.value.input1,
-      notification: this.notificationList,
-      projectId: this._ProjectId,
-    };
-
     const testData: PaymentModel = {
       usage: this.form.value.input1,
-      note: this.form.value.input1,
-      budget: '123',
-      change: true,
+      note: this.form.value.input2,
+      budget: this.form.value.input3.toString(),
+      change: this.form.value.input6,
       notification: this.notificationList,
       projectId: this._ProjectId,
     };
 
-
-    this.updatePayment(testData)
+    this.updatePayment(testData);
     console.log(testData);
-    
+    this.notificationList = [];
+    this.form.reset();
+  }
+
+  incBudget() {
+    this.form.get('input3')?.setValue(this.form.get('input3')?.value + 1000);
+
+    console.log(this.form.value.input3);
+  }
+
+  dcBudget() {
+    if (this.form.value.input3 - 1000 >= 0) {
+      this.form.get('input3')?.setValue(this.form.get('input3')?.value - 1000);
+    }
+
+    console.log(this.form.value.input3);
   }
 }
