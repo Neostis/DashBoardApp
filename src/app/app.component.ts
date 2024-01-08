c18d71651cdeff7d96c25ead36afc16b687c504e
Jarpip <naluedon.a@gmail.com>
2024-01-08 14:27:04 +0700

    Clean Unuse Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IonRouterOutlet } from '@ionic/angular/standalone';
 import { IonicModule } from '@ionic/angular';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,22 +9,6 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [IonicModule, IonRouterOutlet, RouterLink],
 })
-export class AppComponent implements OnInit {
-  route: string = 'home';
-
+export class AppComponent {
   constructor() {}
-
-  ngOnInit() {
-    const storedRoute = localStorage.getItem('currentRoute');
-    if (storedRoute) {
-      this.route = storedRoute;
-    } else {
-      localStorage.setItem('currentRoute', this.route.toLowerCase());
-    }
-  }
-
-  setRoute(value: string) {
-    localStorage.setItem('currentRoute', value.toLowerCase());
-    this.route = value.toLowerCase();
-  }
 }
